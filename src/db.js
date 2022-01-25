0bcf76c6daab52aef1d2d82673ffa9bac6c4a461
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2022-01-25 16:14:40 +0000

    feat(db): connect to db

--- src/db.js
+++ src/db.js
@@ -1,21 +1,10 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
 
-const firebaseConfig = {
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
-};
-
 admin.initializeApp({
-  credential: applicationDefault(),
+  projectId: process.env.FIREBASE_PROJECT_ID,
 });
 
-const db = admin.getFirestore(); //?
+const db = admin.firestore();
 
-export default db;
+module.exports = {};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+const db = require('./db');
 
 //fire up the app
 (async () => {
