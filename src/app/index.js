8b56051cafabd5afb6f1aa7c1fd1a9fc52a7f489
JIHYO-JEON <jihyo@gm.gist.ac.kr>
2022-02-03 16:26:26 +0000

    fix(app): disable socket mode

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,6 @@ const CronJob = require('cron').CronJob;
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
   port: process.env.PORT || 3000,
 });
